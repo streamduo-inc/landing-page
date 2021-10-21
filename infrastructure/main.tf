4ddfd7bec2cbf0c71c521ed12ce7e359c27a62d4
steve frensch <stevefrensch@steves-MacBook-Pro.local>
2021-10-21 09:21:31 -0400

    adding files

---
+++ infrastructure/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  backend "remote" {
+    organization = "sfrensch"
+
+    workspaces {
+      name = "front-end"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "admin.streamduo.io"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.domain_name
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.website_policy.json
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+data "aws_iam_policy_document" "website_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::${var.domain_name}/*"
+    ]
+  }
+}
