35e34ba9a592c83c40f3b730bd1602429248bb40
steve frensch <sfrensch@gmail.com>
2022-02-15 10:06:08 -0500

    adding IO bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,3 +41,34 @@ data "aws_iam_policy_document" "website_policy" {
     ]
   }
 }
+
+
+variable "domain_name_io" {
+  type    = string
+  default = "streamduo.io"
+}
+
+resource "aws_s3_bucket" "landing_bucket_io" {
+  bucket = var.domain_name
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.website_policy_io.json
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+data "aws_iam_policy_document" "website_policy_io" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::${var.domain_name_io}/*"
+    ]
+  }
+}
